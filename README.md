2672e6e6887c756b220cdb13c03a84ec7d6f11cf
prettyairline11 <69813402+prettyairline11@users.noreply.github.com>
2021-09-09 14:04:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# xoxo
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,14 @@
+var gameState=0
+var playerCount=0
+
+var db
+function setup() {
+  createCanvas(400,800);
+  console.log(database) 
+ //  db=database.
+}
+
+function draw() {
+  background(255,255,255);  
+  
+}
